81bab03a8743593498ea4735d7b7a79955e52260
Avi P <avip@Avis-MacBook-Pro.local>
2018-08-14 16:41:57 -0500

    Form Changes

--- headlines.py
+++ headlines.py
@@ -1,26 +1,33 @@
 import feedparser
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-RSS_Feed = {'bbc_feed' : "http://feeds.bbci.co.uk/news/rss.xml",
-            'cnn_feed' : "http://rss.cnn.com/rss/edition.rss",
-            'fox_feed' : "http://feeds.foxnews.com/foxnews/latest"}
+RSS_Feeds = {'bbc' : "http://feeds.bbci.co.uk/news/rss.xml",
+            'cnn' : "http://rss.cnn.com/rss/edition.rss",
+            'fox' : "http://feeds.foxnews.com/foxnews/latest"}
 
 @app.route('/')
 def index():
     return "To view headlines, please go to http://localhost/bbc"
 
-@app.route('/<publication>')
-def get_news(publication="bbc"):
+@app.route('/search')
+def get_news():
 
-    publication = publication + '_feed'
+    pub = request.args.get('pub')
+    print pub
+    if not pub or pub.lower() not in RSS_Feeds:
+        publication = 'bbc'
+    else:
+        publication = pub.lower()
 
-    feed = feedparser.parse(RSS_Feed[publication])
+    print 'pub : ' + publication
+
+    feed = feedparser.parse(RSS_Feeds[publication])
     #first_article = feed['entries'][0]
     articles = feed['entries']
 
-    return render_template('news.html', articles=articles)
+    return render_template('news.html', articles=articles, publication=publication)
        # </html>.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
 
 if __name__ == "__main__":
