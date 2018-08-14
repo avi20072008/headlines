9cfb1568b9be9b731718d11f9b8f61466a3fa159
Avi P <avip@Avis-MacBook-Pro.local>
2018-08-13 22:27:33 -0500

    dynamic routing

--- headlines.py
+++ headlines.py
@@ -1,6 +1,28 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf830
-{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-}
+import feedparser
+from flask import Flask
+
+app = Flask(__name__)
+
+RSS_Feed = {'bbc_feed' : "http://feeds.bbci.co.uk/news/rss.xml",
+            'cnn_feed' : "http://rss.cnn.com/rss/edition.rss",
+            'fox_feed' : "http://feeds.foxnews.com/foxnews/latest"}
+
+@app.route('/<publication>')
+def get_news(publication="bbc"):
+
+    publication = publication + '_feed'
+
+    feed = feedparser.parse(RSS_Feed[publication])
+    first_article = feed['entries'][0]
+
+    return """<html>
+            <body>
+                <h1> BBC Headlines </h1>
+                <b>{0}</b> <br/>
+                <i>{1}</i> <br/>
+                <p>{2}</p> <br/>
+            </body>
+        </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+
+if __name__ == "__main__":
+  app.run(port=5000, debug=True)
